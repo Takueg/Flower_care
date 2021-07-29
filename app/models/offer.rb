0018c4d8a533d20e6ced40f248de43263408aa8e
Torsten Tyras <tyras.torsten@gmail.com>
2021-07-29 20:37:07 +0200

    offer validations

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,3 +1,10 @@
 class Offer < ApplicationRecord
   belongs_to :user
+
+# validates :title, length: {minimum: 15}
+  validates :address, presence: true
+  validates :description, length: {minimum: 15}
+  validates :instruction, presence: true
+
+
 end
