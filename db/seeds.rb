13a81e2840da6d49838b209c28b653bed34e30dd
Takudzwa Gwindingwi <takueg@gmail.com>
2021-07-29 20:33:58 +0200

    installing faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# we need to create 5 offers
+# with a title, image_url,address, description, instructions, date and price
+
+puts "Cleaning database..."
+Offer.destroy_all
+
+puts "creating offers"
+
+
+images = []
+
+images.each do |image|
+  5.times do
+    Offer.new(
+      image_url: image,
+      description: Faker::Lorem.paragraph,
+      )
+  end
+end
