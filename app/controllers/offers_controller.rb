182bf3ffb85ec19e1e3cd719b4ecdccbbc652d46
Lena Oberherr <lena.oberherr@gmail.com>
2021-07-31 11:45:59 +0200

    worked on no  login for index

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,5 +1,5 @@
 class OffersController < ApplicationController
-    skip_before_action :authenticate_user!, only: [:home, :show, :index]
+    skip_before_action :authenticate_user!, only: [ :show, :index]
     before_action :set_offer, only: [ :show ]
     
     def index
