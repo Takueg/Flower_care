abecc48a71a5b99b86a81b6fd52e8501dce57293
Lena Oberherr <lena.oberherr@gmail.com>
2021-07-31 20:32:44 +0200

    added image upload to offer new

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,6 +1,7 @@
 class Offer < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_one_attached :image_url
 
 # validates :title, length: {minimum: 15}
   validates :address, presence: true
