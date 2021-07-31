2beef3b2ab03a7b3980b4badab476ee91cb561c6
Lena Oberherr <lena.oberherr@gmail.com>
2021-07-31 11:37:06 +0200

    added booking model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :offers
+  has_many :bookings
 
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
