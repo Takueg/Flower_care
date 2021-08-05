13104af4e73fa2591b05a806f122d3b6f2c1bd91
Torsten Tyras <tyras.torsten@gmail.com>
2021-08-05 18:47:43 +0200

    integrated the booking-feature - 1st steps, so that it works

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'offers#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :offers, only: [:index, :create, :new, :show] do
-    resources :booking, only: [ :new, :create ]
+    resources :bookings, only: [ :new, :create ]
   end
 end
