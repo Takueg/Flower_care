08b8db897ab940fc14b501f1e84608af853da669
Takudzwa Gwindingwi <takueg@gmail.com>
2021-08-07 14:59:12 +0200

    map issue sorted

--- app/javascript/packs/change_button.js
+++ app/javascript/packs/change_button.js
@@ -1,12 +1,14 @@
 const changeButton = (event) => {
   const button = document.querySelector('.booking-button');
-  button.addEventListener('click', (event) => {
-  // Callback
-  event.currentTarget.value = 'Booked';
-  event.currentTarget.setAttribute("disabled", "");
-  console.log("button is clicked");
-  console.log(button);
-  });
+  if (button) {
+    button.addEventListener('click', (event) => {
+    // Callback
+    event.currentTarget.value = 'Booked';
+    event.currentTarget.setAttribute("disabled", "");
+    console.log("button is clicked");
+    console.log(button);
+    });
+  }
 }
 
 export { changeButton };
--- app/javascript/plugins/init_sweetalert.js
+++ app/javascript/plugins/init_sweetalert.js
@@ -1,4 +1,4 @@
-import swal from 'sweetalert';
+import swal from 'sweetalert2';
 
 const initSweetalert = (selector, options = {}) => {
   const swalButton = document.querySelector('.booking-button');
